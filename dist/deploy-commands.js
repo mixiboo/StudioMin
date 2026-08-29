07c45d4fa8fe445c4dcdc3ee1ce1e7019d911b91
민준 <mixi.boo.10@gmail.com>
2026-08-29 20:51:28 +0900

    fix: restore compiled global command deployment

--- dist/deploy-commands.js
+++ dist/deploy-commands.js
@@ -6,10 +6,8 @@ const config_1 = require("./config");
 const commands_1 = require("./commands");
 const commandsData = Object.values(commands_1.commands).map((command) => command.data.toJSON());
 const rest = new discord_js_1.REST({ version: "10" }).setToken(config_1.config.DISCORD_TOKEN);
-const GUILD_ID = process.env.DISCORD_GUILD_ID;
 async function deployCommands() {
-    if (!GUILD_ID) throw new Error("Missing DISCORD_GUILD_ID environment variable");
-    console.log("Started refreshing application (/) commands for the server.");
-    await rest.put(discord_js_1.Routes.applicationGuildCommands(config_1.config.DISCORD_CLIENT_ID, GUILD_ID), { body: commandsData });
-    console.log("Successfully reloaded application (/) commands for the server.");
+    console.log("Started refreshing application (/) commands globally.");
+    await rest.put(discord_js_1.Routes.applicationCommands(config_1.config.DISCORD_CLIENT_ID), { body: commandsData });
+    console.log("Successfully reloaded application (/) commands globally.");
 }
