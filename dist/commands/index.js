7ead33d00418978d0d751ff2dd87636a220a9603
민준 <mixi.boo.10@gmail.com>
2026-08-29 20:50:12 +0900

    fix: sync dist registry with message and embed commands

--- dist/commands/index.js
+++ dist/commands/index.js
@@ -1,37 +1,7 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) { if (k2 === undefined) k2 = k; var desc = Object.getOwnPropertyDescriptor(m, k); if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) { desc = { enumerable: true, get: function() { return m[k]; } }; } Object.defineProperty(o, k2, desc); }) : (function(o, m, k, k2) { if (k2 === undefined) k2 = k; o[k2] = m[k]; }));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) { Object.defineProperty(o, "default", { enumerable: true, value: v }); }) : function(o, v) { o["default"] = v; });
+var __importStar = (this && this.__importStar) || (function () { var ownKeys = function(o) { ownKeys = Object.getOwnPropertyNames || function (o) { var ar = []; for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k; return ar; }; return ownKeys(o); }; return function (mod) { if (mod && mod.__esModule) return mod; var result = {}; if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]); __setModuleDefault(result, mod); return result; }; })();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.commands = void 0;
 const ping = __importStar(require("./ping"));
@@ -45,16 +15,6 @@ const reactionRolePanel = __importStar(require("./roles/reaction-role-panel"));
 const purge = __importStar(require("./moderation/purge"));
 const kick = __importStar(require("./moderation/kick"));
 const ban = __importStar(require("./moderation/ban"));
-exports.commands = {
-    ping,
-    "로그-설정": logSettings,
-    "welcome-setup": welcomeSetup,
-    "leave-setup": leaveSetup,
-    "reaction-role-add": reactionRoleAdd,
-    "reaction-role-remove": reactionRoleRemove,
-    "reaction-role-list": reactionRoleList,
-    "reaction-role-panel": reactionRolePanel,
-    purge,
-    kick,
-    ban
-};
+const message = __importStar(require("./message"));
+const embed = __importStar(require("./embed"));
+exports.commands = { ping, "로그-설정": logSettings, "welcome-setup": welcomeSetup, "leave-setup": leaveSetup, "reaction-role-add": reactionRoleAdd, "reaction-role-remove": reactionRoleRemove, "reaction-role-list": reactionRoleList, "reaction-role-panel": reactionRolePanel, purge, kick, ban, message, embed };
